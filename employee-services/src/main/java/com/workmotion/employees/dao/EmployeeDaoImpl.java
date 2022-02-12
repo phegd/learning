7a3294e8fb2f92a48ede94724fd20cc2a87fd259
Prasad Ramakrishna Hegde <prasad.tumbemane@gmail.com>
2022-02-12 15:04:30 +0530

    2 - Code Improvements

---
+++ employee-services/src/main/java/com/workmotion/employees/dao/EmployeeDaoImpl.java
@@ -0,0 +1,29 @@
+package com.workmotion.employees.dao;
+
+import com.workmotion.employees.entities.EmployeeDetails;
+import org.springframework.stereotype.Repository;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Repository
+public class EmployeeDaoImpl implements EmployeeDao {
+    //TODO: Temporary in-memory data structure used instead of database table
+    private Map<Integer, EmployeeDetails> empDetailsMap = new ConcurrentHashMap<>();
+    private final Integer EMP_ID_SEQ_DEFAULT = 10000;
+
+    @Override
+    public Integer saveEmployee(EmployeeDetails empDetails) {
+        if(empDetails.getEmpId() == null) {
+            empDetails.setEmpId((empDetailsMap.isEmpty() ? EMP_ID_SEQ_DEFAULT : Collections.max(empDetailsMap.keySet())) + 1);
+        }
+        empDetailsMap.put(empDetails.getEmpId(), empDetails);
+        return empDetails.getEmpId();
+    }
+
+    @Override
+    public EmployeeDetails findEmployee(Integer empId) {
+        return empDetailsMap.get(empId);
+    }
+}
